2a2acc70eed7d882305f4b4ae4ea005975414302
JEB <j.e.b.inter@gmail.com>
2025-09-16 20:50:48 -0300

    subiendo carpetas

---
+++ js/app.js
@@ -0,0 +1,15 @@
+
+const imagenes = document.querySelectorAll('.propiedad__imagen');
+// console.log(imagenes);
+
+
+
+
+window.addEventListener('scroll', ()=>{
+    // console.log(this.scrollY);
+    const scroll = this.scrollY / -20;
+
+        imagenes.forEach((imagen)=>{
+        imagen.style.backgroundPositionY = `${scroll}px`;
+    })
+})
